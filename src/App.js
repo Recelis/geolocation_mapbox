76f2175973fba3f34f05bc072c7ba412250c7f7a
Jacky Lui <jackymlui@live.com>
2023-06-20 20:11:42 +1000

    first commit exploration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const geolocationAPI = navigator.geolocation
+  
+  
+  const [location, setLocation] = useState(undefined)
+  // https://www.openstreetmap.org/search?query=brisbane#map=19/-27.46907/153.02576
+  // https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
+  // https://www.mapbox.com/mapbox-gljs
+  useEffect(() => {
+  geolocationAPI.getCurrentPosition((gelocation) => {
+    console.log(gelocation)
+    setLocation(gelocation)
+  })}
+  ,
+  [geolocationAPI])
+  console.log(location)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {location?.coords && 
+          <a href={`https://www.openstreetmap.org/#map=18/${location.coords.latitude}/${location.coords.longitude}`}>
+            {`Latitude: ${location.coords.latitude} °, Longitude: ${location.coords.longitude} °`}
+            </a>
+        }
       </header>
     </div>
   );
